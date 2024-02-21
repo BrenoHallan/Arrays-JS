8a4f4c99acd30303272b8223b5d8cdf4c87e9c97
BrenoHallan <brenohmt88@hotmail.com>
2024-02-21 14:26:35 -0300

    arquivos removidos ...

--- Array-calculo-media.js
+++
@@ -1,8 +0,0 @@
-//crie uma constante (const) para armazenar valores numericos inteiros ou valores nao inteiros
-const valores = [10, 2, 8]
-
-const media = (valores[0] + valores[1] + valores[2]) / valores.length
-//                     ^                    ^                     ^
-//   posicao do valor no array      nome do array       comprimento do array
-
-console.log(media)
--- Array-remover-valores.js
+++
@@ -1,8 +0,0 @@
-// arrays declaradas em uma constante podem sofrer modificaçoes, mesmo que seja uma constante.
-
-const ArrayX = [1,2,3]
-
-ArrayX.pop()
-
-console.log(ArrayX)
-// saida: [1,2]
